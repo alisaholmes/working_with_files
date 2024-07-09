cb3ac1399a8e1042ad6d7183372de63c7db54375
Julia Isaacson <alisa.holmes@mail.ru>
2024-07-10 02:17:40 +0300

    com

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,7 +1,9 @@
 import os
 import shutil
-import pytest
 import zipfile
+
+import pytest
+
 from script_os import FILES_DIR, ZIP_DIR, ARCHIVE_DIR
 
 
@@ -10,7 +12,7 @@ def create_archive():
     if not os.path.exists(ARCHIVE_DIR):
         os.mkdir(ARCHIVE_DIR)
         with zipfile.ZipFile(ZIP_DIR, 'w') as zf:  # создаем архив
-            for file in os.listdir(FILES_DIR):  # добавляем файлы в архив
+            for file in os.listdir(FILES_DIR):
                 zf.write(os.path.join(FILES_DIR, file), file)  # добавляем файл в архив
 
     # удаление_файлов_после_архивации
--- tests/test_homework7.py
+++ tests/test_homework7.py
@@ -1,41 +1,35 @@
 import csv
 import zipfile
-from pypdf import PdfReader
-from script_os import ZIP_DIR
-from openpyxl import load_workbook
-import datetime
 
+from openpyxl import load_workbook
+from pypdf import PdfReader
+
+from script_os import ZIP_DIR
 
 
 def test_pdf():
-        with zipfile.ZipFile(ZIP_DIR) as zip_file:
-                with zip_file.open('KPI.pdf') as pdf_file:
-                        reader = PdfReader(pdf_file)
-                        number_of_pages = len(reader.pages)
-                        print(number_of_pages)
-                        page = reader.pages[0]
-                        text = page.extract_text()
-                assert 'Качественный показатель' in text
-
+    with zipfile.ZipFile(ZIP_DIR) as zip_file:
+        with zip_file.open('KPI.pdf') as pdf_file:
+            reader = PdfReader(pdf_file)
+            page = reader.pages[0]
+            text = page.extract_text()
+            assert 'Качественный показатель' in text
 
 
 def test_xlsx():
-        with zipfile.ZipFile(ZIP_DIR) as zip_f:
-                with zip_f.open('characters.xlsx') as xlsx_file:
-                        workbook = load_workbook(xlsx_file)
-                        sheet = workbook.active
-                        for x in sheet.columns:
-                                assert sheet['B7'].value == 'Берест Владимир'
-                                assert sheet['C7'].value == datetime.datetime(1999, 1, 5, 0, 0)
-                                assert sheet['D4'].value == 'Первое появление в главе'
+    with zipfile.ZipFile(ZIP_DIR) as zip_f:
+        with zip_f.open('characters.xlsx') as xlsx_file:
+            workbook = load_workbook(xlsx_file)
+            sheet = workbook.active
+            value = sheet.cell(row=7, column=2).value
+            name = 'Берест Владимир'
+            assert name in value
 
 
 def test_csv():
-        with zipfile.ZipFile(ZIP_DIR) as zip_f:
-                with zip_f.open('characters_story.csv') as csv_file:
-                        content = csv_file.read().decode('utf-8-sig')
-                        csvreader = list(csv.reader(content.splitlines()))
-                        second_row = csvreader[1]
-                        assert second_row[0] == 'Беликов Юрий;14.06.2001;глава 1'
-
-
+    with zipfile.ZipFile(ZIP_DIR) as zip_f:
+        with zip_f.open('characters_story.csv') as csv_file:
+            content = csv_file.read().decode('utf-8-sig')
+            csvreader = list(csv.reader(content.splitlines()))
+            second_row = csvreader[1]
+            assert second_row[0] == 'Беликов Юрий;14.06.2001;глава 1'
